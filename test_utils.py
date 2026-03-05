b4b09adbf9b6c4d6ab6d3cc957c03e13613f61e5
Giff21 <24087@ecam.be>
2026-03-05 13:27:32 +0100

    Add unit test for utils module

---
+++ test_utils.py
@@ -0,0 +1,14 @@
+import pytest
+import utils
+
+def test_fact():
+    number = 2
+    pass
+
+def test_roots():
+    equ = x^2 + 2x + 1
+    pass
+
+def test_integrate():
+    function = 'x ** 2 - 1', -1, 1
+    pass
